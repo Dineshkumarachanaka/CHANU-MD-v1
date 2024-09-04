d9ed2b5e42efb79b47d318357fafb05e4fa016dd
Dineshkumarachanaka <137772104+Dineshkumarachanaka@users.noreply.github.com>
2024-09-04 09:24:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,22 +11,22 @@ global.github = 'https://github.com/Dineshkumarachanaka/CHANU-MD-v1'
 global.location = 'Sultanpur IN'
 global.gurl = 'https://instagram.com/' // add your username
 global.devs = '94762280384';
-global.website = 'https://github.com/prabathLK/PRABATH-MD' //wa.me/+94762280384
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.imgur.com/H062oEI.jpeg'
+global.website = 'https://github.com/Dineshkumarachanaka/CHANU-MD-v1' //wa.me/+94761438198
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || ','
 module.exports = {
   OPENAI_API_KEY:process.env.OPENAI_API_KEY || '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',
-  botname: process.env.BOT_NAME || 'PRABATH-MD',
+  botname: process.env.BOT_NAME || 'CHANU-MD',
   dl_limit: process.env.DL_SIZE || '300',
   art: process.env.AUTO_RESTART || 'true',
-  sudo: process.env.SUDO || '94762280384',
-  alivepic: process.env.ALIVE_IMG || 'https://telegra.ph/file/ad25b2227fa2a1a01b707.jpg',
-  alivemsj: process.env.ALIVE_MSJ || '*POWERD BY PRABATH-MD* 🎊',
-  ownername:process.env.OWNER_NAME || 'PRABATH',
+  sudo: process.env.SUDO || '94761438198',
+  alivepic: process.env.ALIVE_IMG || 'https://telegra.ph/file/d2e43e0b7e028a334be7a.jpg',
+  alivemsj: process.env.ALIVE_MSJ || '*POWERD BY CHANU-MD* 🎊',
+  ownername:process.env.OWNER_NAME || 'CHANU',
   sessionName: process.env.SESSION_ID || 'PUT-HERE',
   author: process.env.PACK_INFO.split(";")[0] || 'author', 
   auto_read_status : process.env.AUTO_READ_STATUS || 'false',
   packname: process.env.PACK_INFO.split(";")[1] || 'Name',
-  autoreaction: process.env.AUTO_REACTION || 'off',
+  autoreaction: process.env.AUTO_REACTION || 'on',
   antibadword : process.env.ANTI_BAD_WORD || 'fuck,huth,fucking,huththa,huththo,hukanna,fucked,pakaya,pako,ponnaya,ponnayo,kariya,kariyo,huththaa,hutta',
   alwaysonline: process.env.ALWAYS_ONLINE || 'true',
   antifake : process.env.FAKE_COUNTRY_CODE || '',
